3e6d9a5d57aad6bab90d5cf00d8d75463d4263e7
Jany Muong <me.roumuong@gmail.com>
2023-10-10 19:15:20 +0300

    changed approach; discard memo arg

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,33 +8,31 @@ Your text editor can execute only two operations in this file:
 '''
 
 
-def minOperations(n, memo={}):
+def minOperations(n):
     '''minOperations(args)
     calculates the fewest number of operations
     needed to achieve `n` `H` characters in a text file.
 
     args:
     n: the desired number of H characters in the text file.
-    memo: dictionary to store already computed results,
-        which significantly reduces the number of recursive calls
 
     returns:
         an integer, the fewest number of operations
         needed to achieve n H characters in the text file,
         or 0 if n is impossible to achieve.
     '''
-    if n == 1:
-        return 1
-    elif n < 1:
+    if n <= 0:
         return 0
 
-    if n in memo:
-        return memo[n]
+    # initialize array to store minimum operations for each position
+    min_op = [float('inf')] * (n + 1)
 
-    half = n // 2
-    if n % 2 == 0:
-        memo[n] = min(minOperations(half) + 1, minOperations(n - half) + 1)
-    else:
-        memo[n] = min(minOperations(half) + 2, minOperations(n - half) + 2)
+    min_op[1] = 0
 
-    return memo[n]
+    # get minimum operations for each position from 2 to n
+    for i in range(2, n + 1):
+        for j in range(1, i):
+            if i % j == 0:
+                min_op[i] = min(min_op[i], min_op[j] + i // j)
+
+    return min_op[n] if min_op[n] != float('inf') else 0
