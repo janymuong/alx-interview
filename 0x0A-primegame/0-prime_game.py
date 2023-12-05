62b78d5531eef138e54a6cea34066524847d78dc
Jany Muong <me.roumuong@gmail.com>
2023-12-05 07:41:47 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -11,68 +11,29 @@ def isWinner(x, nums):
         name of the player that won the most rounds,
         else return None
     '''
-
     mariaWins = 0
     benWins = 0
-
-    for _ in range(x):
-        mariaTurn = True
-        n = len(nums)
-
-        while n > 0:
-            if mariaTurn:
-                prime = findPrime(nums)
-                removeMultiples(nums, prime)
-                n -= 1
-                mariaTurn = False
-            else:
-                prime = findPrime(nums)
-                removeMultiples(nums, prime)
-                n -= 1
-                mariaTurn = True
-
-        if mariaTurn:
+    for i in range(x):
+        primes = find_prime(nums[i])
+        if len(primes) % 2 == 0:
             benWins += 1
         else:
             mariaWins += 1
-
-    if mariaWins > benWins:
+    if mariaWins > mariaWins:
         return f'Maria'
     elif benWins > mariaWins:
         return f'Ben'
-    else:
-        return None
+    return None
 
 
-def findPrime(nums):
+def find_prime(n):
     '''return prime number determined:
     '''
-    for num in nums:
-        if isPrime(num):
-            return num
-    return -1
-
-
-def removeMultiples(nums, prime):
-    '''rid multiples/ duplicates
-    '''
-    for i in range(len(nums) - 1, -1, -1):
-        if nums[i] % prime == 0:
-            del nums[i]
-
-
-def isPrime(n):
-    '''get prime numbers
-    '''
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+    primes = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primes.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primes
