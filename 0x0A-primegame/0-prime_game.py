e45bc69a9c4bd4ceb8ddea50a103f875d191d664
Jany Muong <me.roumuong@gmail.com>
2023-12-05 07:56:44 +0300

    prime game - return `None` for null args

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,6 +3,19 @@
 '''
 
 
+def find_prime(n):
+    '''return prime number determined:
+    '''
+    primes = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primes.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primes
+
+
 def isWinner(x, nums):
     '''prime game implementation
     args
@@ -27,16 +40,3 @@ def isWinner(x, nums):
     elif benWins > mariaWins:
         return f'Ben'
     return None
-
-
-def find_prime(n):
-    '''return prime number determined:
-    '''
-    primes = []
-    filtered = [True] * (n + 1)
-    for prime in range(2, n + 1):
-        if (filtered[prime]):
-            primes.append(prime)
-            for i in range(prime, n + 1, prime):
-                filtered[i] = False
-    return primes
